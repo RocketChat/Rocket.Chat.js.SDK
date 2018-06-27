be6855e333907cef7a6a8154f189bba38da9ca84
Tim Kinnane <timkinnane@gmail.com>
2018-06-27 14:53:44 +1000

    fix(setReaction): Replace test emoji with argument

--- src/lib/driver.ts
+++ src/lib/driver.ts
@@ -592,5 +592,5 @@ export function editMessage (message: IMessage): Promise<IMessage> {
  * @param messageId ID for a previously sent message
  */
 export function setReaction (emoji: string, messageId: string) {
-  return asyncCall('setReaction', [':punch:', messageId])
+  return asyncCall('setReaction', [emoji, messageId])
 }
