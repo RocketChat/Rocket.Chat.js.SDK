906b035932fb28780b497662c7334519bf4df7f3
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-12-19 16:00:49 -0200

    convert https? to  wss?

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -22,7 +22,6 @@ export function debounce<F extends Procedure> (
 }
 
 /** Convert a http/s protocol address to a websocket URL */
-export function hostToWS (host: string, ssl = false) {
-  host = host.replace(/^(https?:\/\/)?/, '')
-  return `ws${ssl ? 's' : ''}://${host}`
+export function hostToWS (host: string) {
+  return host.replace(/^http/, 'ws')
 }
