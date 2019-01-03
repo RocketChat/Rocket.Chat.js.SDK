4b1a61d761c4bf5526d1a7b98572cefc46cbf61e
Diego Sampaio <chinello@gmail.com>
2019-01-03 17:25:38 -0200

    Fix hostToWS logic

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -22,6 +22,7 @@ export function debounce<F extends Procedure> (
 }
 
 /** Convert a http/s protocol address to a websocket URL */
-export function hostToWS (host: string) {
-  return host.replace(/^http/, 'ws')
+export function hostToWS (host: string, ssl = false) {
+  host = host.replace(/^(https?:\/\/)?/, '')
+  return `ws${ssl ? 's' : ''}://${host}`
 }
