6c833e19f577d2455a5ced495648d6dda095fa4a
Renato Becker <renato.augusto.becker@gmail.com>
2019-04-16 00:53:21 -0300

    Replace param interface.

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -619,12 +619,6 @@ export interface INewLivechatRoomCredentialAPI {
   agentId?: string
 }
 
-/** Structure to get livechat message */
-export interface ILivechatMessageCredentialAPI {
-  rid: string
-  token: string
-}
-
 /** Structure for livechat room messages api */
 export interface ILivechatRoomMessagesAPI {
   token: string   // Visitor token
