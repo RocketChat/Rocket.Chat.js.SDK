a3243f2f76a2f905904be529b9a4b1db2aadae62
Diego Mello <diegolmello@gmail.com>
2026-08-31 17:19:15 -0300

    test: assert the settings re-export without assigning to an imported binding

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -2,15 +2,9 @@ import * as sdk from '../index'
 import * as settings from '../lib/settings'
 
 describe('SDK entry point', () => {
-  afterEach(() => {
-    settings.customHeaders = {}
-  })
+  it('re-exports the live settings module rather than a copy of it', () => {
+    jest.replaceProperty(settings, 'customHeaders', { 'X-Test': 'value' })
 
-  it('exports settings as the same mutable module namespace', () => {
-    expect(sdk.settings.customHeaders).toEqual({})
-
-    sdk.settings.customHeaders = { 'X-Test': 'value' }
-
-    expect(settings.customHeaders).toEqual({ 'X-Test': 'value' })
+    expect(sdk.settings.customHeaders).toEqual({ 'X-Test': 'value' })
   })
 })
